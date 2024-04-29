aa02ae22e29296ba4fefa0194df071402472071d
Maddouri-Ahmed <145608692+Maddouri-Ahmed@users.noreply.github.com>
2024-04-29 19:11:59 +0100

    maddouriCommit

--- coco/src/app/app-routing.module.ts
+++ coco/src/app/app-routing.module.ts
@@ -18,7 +18,26 @@ import { MesAnnoncesComponent } from './annoceColoc/mes-annonces/mes-annonces.co
 import { AnnoceColocViewComponent } from './annoceColoc/annoce-coloc-view/annoce-coloc-view.component';
 import { UpdateAnnonceColocComponent } from './annoceColoc/update-annonce-coloc/update-annonce-coloc.component';
 import { CreateReservationColocComponent } from './reservation/create-reservation-coloc/create-reservation-coloc.component';
-
+  //MaddouriCovoiturage
+  import { AnnonceComponent } from './covoiturage/components/annonce/annonce.component';
+import { VoitureComponent } from './covoiturage/components/voiture/voiture.component';
+import { AjoutervoitureComponent } from './covoiturage/components/ajoutervoiture/ajoutervoiture.component';
+import { AjouterannonceComponent } from './covoiturage/components/ajouterannonce/ajouterannonce.component';
+import { DetailedAnnonceComponent } from './covoiturage/components/detailed-annonce/detailed-annonce.component';
+import { MesannoncesComponent } from './covoiturage/components/mesannonces/mesannonces.component';
+import { ReservationComponent } from './covoiturage/components/reservation/reservation.component';
+import { BackannonceComponent } from './covoiturage/components/backannonce/backannonce.component';
+import { BackupdateannonceComponent } from './covoiturage/components/backupdateannonce/backupdateannonce.component';
+import { UpdateannonceComponent } from './covoiturage/components/updateannonce/updateannonce.component';
+import { UpdatevoitureComponent } from './covoiturage/components/updatevoiture/updatevoiture.component';
+import { StatComponent } from './covoiturage/components/stat/stat.component';
+import { BackreservationComponent } from './covoiturage/components/backreservation/backreservation.component';
+import { BackvoitureComponent } from './covoiturage/components/backvoiture/backvoiture.component';
+import { BackupdatevoitureComponent } from './covoiturage/components/backupdatevoiture/backupdatevoiture.component';
+import { BackcommentComponent } from './covoiturage/components/backcomment/backcomment.component';
+import { NotificationComponent } from './covoiturage/components/notification/notification.component';
+import { StatisComponent } from './covoiturage/components/statis/statis.component';
+import { DashboardsComponent } from './covoiturage/components/dashboards/dashboards.component';
 
 const routes: Routes = [
 
@@ -27,17 +46,36 @@ const routes: Routes = [
   {path:"profile" , component:ProfileComponent},
   {path:"update" , component:UpdateComponent},
   {path:"password" , component:SetPasswordComponent},
-  {path:"back" , component:DashboardComponent},
+  {path:"back" , component:DashboardsComponent},
   {path:"login" , component:LoginComponent},
   { path: 'chat/:userId', component: ChatComponent },
-  { path: "annoncesColoc", component: AnnonceColocListComponent }, // Utilisez le bon nom de composant
-  { path: "annoncesColoc/add", component: CreateAnnonceColocComponent }, // Utilisez le bon nom de composant
-  { path: "annoncesColoc/mesannonces", component: MesAnnoncesComponent }, // Utilisez le bon nom de composant
-  { path: "annoncesColoc/view/:id", component: AnnoceColocViewComponent }, // Utilisez le bon nom de composant
-  { path: "annoncesColoc/:id", component: UpdateAnnonceColocComponent }, // Utilisez le bon nom de composant
+  { path: "annoncesColoc", component: AnnonceColocListComponent }, 
+  { path: "annoncesColoc/add", component: CreateAnnonceColocComponent }, 
+  { path: "annoncesColoc/mesannonces", component: MesAnnoncesComponent }, 
+  { path: "annoncesColoc/view/:id", component: AnnoceColocViewComponent }, 
+  { path: "annoncesColoc/:id", component: UpdateAnnonceColocComponent }, 
   { path: "annoncesColoc/view/:id/reservationColoc", component: CreateReservationColocComponent },
 
 
+  //MaddouriCovoiturage
+  {path:"annonceCOV" , component:AnnonceComponent},
+  {path:"voiture" , component:VoitureComponent},
+  {path:"ajoutervoiture" , component:AjoutervoitureComponent},
+  {path:"ajouterannonce" , component:AjouterannonceComponent},
+  { path: 'annonces/:ida', component: DetailedAnnonceComponent },
+  { path: "annonces", component: MesannoncesComponent },
+  { path: "mesReservations", component: ReservationComponent },
+  { path: "bannonce", component: BackannonceComponent },
+  { path: "bupdate/:ida", component: BackupdateannonceComponent },
+  { path: "updatevoiture/:idv", component:UpdatevoitureComponent  },   
+  { path: "updateannonce/:ida", component:UpdateannonceComponent     },
+  { path: "stats", component:StatComponent},                    
+  { path: "reserv", component:BackreservationComponent},     
+  { path: "bvoiture", component:BackvoitureComponent},                    
+  { path: "buvoiture/:idv", component:BackupdatevoitureComponent},   
+  { path: "bcomment", component:BackcommentComponent},                 
+  { path: "not", component:NotificationComponent},                    
+  { path: "stat", component:StatisComponent},    
 
   {path:"aboutus" , component:StatsComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
