403867aec4f38d725282dee57933dce41f9c64b5
Rawef <115536370+Rawef@users.noreply.github.com>
2024-10-02 22:15:01 +0100

    Update ChatMessage.java

--- testEDITIONs/src/main/java/com/example/testeditions/DTO/ChatMessage.java
+++ testEDITIONs/src/main/java/com/example/testeditions/DTO/ChatMessage.java
@@ -11,6 +11,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import java.io.IOException;
 import java.util.Map;
